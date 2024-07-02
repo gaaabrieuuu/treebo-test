e27c7548a67fcb9674be3ed9a3c9f396e8f853a6
gaaabrieuuu <gabriel.antonio2005@outlook.com>
2024-07-02 11:10:07 -0300

    T002: build - Criação de Entity para a task

--- treebo-backend/src/tasks/entities/task.entity.ts
+++ treebo-backend/src/tasks/entities/task.entity.ts
@@ -1,1 +1,19 @@
-export class Task {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 500 })
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @Column('datetime')
+  date: Date;
+
+  @Column()
+  status: boolean;
+}
