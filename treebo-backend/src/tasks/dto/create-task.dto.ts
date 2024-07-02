2be35b91dbac0d34dfeca6b043595143f038a2ed
gaaabrieuuu <gabriel.antonio2005@outlook.com>
2024-07-02 12:26:29 -0300

    T003: feat - Criação de DTO

--- treebo-backend/src/tasks/dto/create-task.dto.ts
+++ treebo-backend/src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,25 @@
-export class CreateTaskDto {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity()
+export class CreateTaskDto {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Column({ length: 500 })
+  title: string;
+
+  @ApiProperty()
+  @Column('text')
+  description: string;
+
+  @ApiProperty()
+  @Column('datetime')
+  date: Date;
+
+  @ApiProperty()
+  @Column()
+  status: boolean;
+}
