82f6f53b9742d50b6ae795fcb1352e0c714add97
gaaabrieuuu <gabriel.antonio2005@outlook.com>
2024-07-18 12:55:09 -0300

    T010: refactor - DTOs transformados em classes

--- treebo-frontend/src/services/dto/create-task.dto.ts
+++ treebo-frontend/src/services/dto/create-task.dto.ts
@@ -1,5 +1,11 @@
-export interface CreateTaskDto {
+export class CreateTaskDto {
   title: string;
   description: string;
   date: Date;
+
+  constructor(title: string, description: string, date: Date) {
+    this.title = title;
+    this.description = description;
+    this.date = date;
+  }
 }
